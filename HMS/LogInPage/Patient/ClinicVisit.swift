0791851914dbcc17e9d806683dd986d23f41fe30
Manpreet Singh <101382758+man-pb11@users.noreply.github.com>
2024-05-30 22:42:44 -0700

    add

---
+++ HMS/LogInPage/Patient/ClinicVisit.swift
@@ -0,0 +1,181 @@
+import SwiftUI
+
+struct ClinicView: View {
+    @State private var selectedDate: String? = nil
+    @State private var selectedTime: String? = nil
+    @State private var treatment: String? = nil
+    @State private var doctor: String? = nil
+    @State private var showingAlert = false
+    @State private var priority: String? = nil
+    @State private var symptomsDescription = ""
+    @State private var navigateToCheckups = false
+
+    let dates = ["Mon 03", "Tue 04", "Wed 05", "Thu 06"]
+    let times = ["08:00 am", "09:00 am", "10:00 am"]
+    let treatments = ["Treatment 1", "Treatment 2", "Treatment 3"]
+    let doctors = ["Doctor A", "Doctor B", "Doctor C"]
+    let priorities = ["Low", "Medium", "High"]
+
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 16) {
+                Form {
+                    Section {
+                        HStack {
+                            Text("Treatment")
+                            Spacer()
+                            Menu {
+                                ForEach(treatments, id: \.self) { treatment in
+                                    Button(action: {
+                                        self.treatment = treatment
+                                    }) {
+                                        Text(treatment)
+                                    }
+                                }
+                            } label: {
+                                HStack {
+                                    Text(treatment ?? "Not Selected")
+                                        .foregroundColor(.green)
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(.green)
+                                }
+                            }
+                        }
+
+                        HStack {
+                            Text("Doctor")
+                            Spacer()
+                            Menu {
+                                ForEach(doctors, id: \.self) { doctor in
+                                    Button(action: {
+                                        self.doctor = doctor
+                                    }) {
+                                        Text(doctor)
+                                    }
+                                }
+                            } label: {
+                                HStack {
+                                    Text(doctor ?? "Not Selected")
+                                        .foregroundColor(.green)
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(.green)
+                                }
+                            }
+                        }
+
+                        HStack {
+                            Text("Priority")
+                            Spacer()
+                            Menu {
+                                ForEach(priorities, id: \.self) { priority in
+                                    Button(action: {
+                                        self.priority = priority
+                                    }) {
+                                        Text(priority)
+                                    }
+                                }
+                            } label: {
+                                HStack {
+                                    Text(priority ?? "Not Selected")
+                                        .foregroundColor(.green)
+                                    Image(systemName: "chevron.right")
+                                        .foregroundColor(.green)
+                                }
+                            }
+                        }
+
+                        Text("Priority patients are charged extra")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+
+                    Section(header: Text("Symptoms description")) {
+                        TextField("What is problem you are facing?", text: $symptomsDescription)
+                    }
+
+                    Section(header: Text("Schedule")) {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack {
+                                ForEach(dates, id: \.self) { date in
+                                    Button(action: {
+                                        selectedDate = date
+                                    }) {
+                                        VStack {
+                                            Text(date.split(separator: " ")[0])
+                                                .font(.headline)
+                                                .foregroundColor(selectedDate == date ? .white : .black)
+                                            Text(date.split(separator: " ")[1])
+                                                .font(.subheadline)
+                                                .foregroundColor(selectedDate == date ? .white : .black)
+                                        }
+                                        .frame(width: 77, height: 77)
+                                        .background(selectedDate == date ? Color.green : Color.white)
+                                        .cornerRadius(8)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 8)
+                                                .stroke(Color.gray.opacity(0.2), lineWidth: 1)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                        .listRowBackground(Color.clear)
+                    }
+
+                    Section(header: Text("Choose time")) {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            LazyHStack {
+                                ForEach(times, id: \.self) { time in
+                                    Button(action: {
+                                        selectedTime = time
+                                    }) {
+                                        Text(time)
+                                            .padding()
+                                            .background(selectedTime == time ? Color.green : Color.white)
+                                            .cornerRadius(35)
+                                            .foregroundColor(selectedTime == time ? .white : .black)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .listRowBackground(Color.clear)
+                }
+
+                Button(action: {
+                    showingAlert = true
+                    // Handle book an appointment action
+                }) {
+                    Text("Book an appointment")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .padding()
+                .alert(isPresented: $showingAlert) {
+                    Alert(
+                        title: Text("Appointment Booked"),
+                        message: Text("You can view your appointment In Check-ups"),
+                        primaryButton: .default(Text("View")) {
+                            navigateToCheckups = true
+                        },
+                        secondaryButton: .cancel(Text("Close"))
+                    )
+                }
+
+                .navigationDestination(isPresented: $navigateToCheckups) {
+                    CheckUpsView()
+                }
+            }
+            .navigationBarTitle("Clinic Visit")
+        }
+    }
+}
+
+struct CLinicView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClinicView()
+    }
+}
---
+++ HMS/LogInPage/Patient/consultationOptions.swift
@@ -0,0 +1,92 @@
+import SwiftUI
+
+struct ConsultationOptionsView: View {
+    let doctor: Doctor
+
+    var body: some View {
+        VStack(spacing: 24) {
+            Text("How would you like to consult\n\(doctor.name)?")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .padding(.top, 20)
+
+            VStack(spacing: 24) {
+                NavigationLink(destination: ClinicView()) {
+                    ConsultationOptionCard(iconName: "plus.circle.fill", title: "Book In-Clinic Consultation", subtitle: "Consult with a General physician In-Clinic")
+                        .frame(minHeight: 150)
+                }
+
+                NavigationLink(destination: VideoChatView()) {
+                    ConsultationOptionCard(iconName: "video.circle.fill", title: "Book In-Video Consultation", subtitle: "Consult with a General physician via Video Call")
+                        .frame(minHeight: 150)
+                }
+            }
+            .padding(.horizontal, 45) // Increased horizontal padding for the cards
+
+            Spacer()
+        }
+
+    }
+}
+
+struct ConsultationOptionCard: View {
+    let iconName: String
+    let title: String
+    let subtitle: String
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.green)
+                .shadow(radius: 5)
+
+            VStack(alignment: .leading, spacing: 0) {
+                Image(systemName: iconName)
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(.white)
+                    .padding(16)
+
+                Spacer()
+
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.headline)
+                        .foregroundColor(.white)
+
+                    Text(subtitle)
+                        .font(.subheadline)
+                        .foregroundColor(.white.opacity(0.8))
+                }
+                .padding([.leading, .bottom, .trailing], 16)
+                .padding(.top, 8)
+            }
+        }
+        .frame(height: 150)
+    }
+}
+
+// Example destination views
+struct InClinicConsultationView: View {
+    var body: some View {
+        Text("In-Clinic Consultation")
+            .navigationBarTitle("In-Clinic Consultation", displayMode: .inline)
+    }
+}
+
+struct VideoConsultationView: View {
+    var body: some View {
+        Text("Video Consultation")
+            .navigationBarTitle("Video Consultation", displayMode: .inline)
+    }
+}
+
+
+struct ConsultationOptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ConsultationOptionsView(doctor: Doctor(name: "Elizabeth", reviews: 5, specialty: "eye surgeon"))
+        }
+    }
+}
